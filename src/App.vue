f336027da132e6c1e567ce07649e106fab546bb8
John Norris <john.n@cyberdime.io>
2025-05-30 13:47:18 -0700

    Updated Top Bar title. Route checked for title and icon.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref, onMounted, onUnmounted } from "vue";
+import { ref, onMounted, onUnmounted, computed } from "vue";
+import { useRoute } from "vue-router";
 import "@mdi/font/css/materialdesignicons.css";
 
 const drawer = ref(false);
@@ -28,20 +29,49 @@ onMounted(() => {
 onUnmounted(() => {
   window.removeEventListener("resize", handleResize);
 });
+
+// Dynamic page title based on route
+const route = useRoute();
+const routeTitles = {
+  "/calculator/standard": "Calculator",
+  "/calculator/fix-flip": "Fix & Flip",
+  "/calculator/buy-hold": "Buy & Hold",
+  "/calculator/brrr": "BRRR",
+  "/calculator/wholesale": "Wholesale",
+  "/calculator/noi": "Net Operating Income",
+  "/calculator/cash-on-cash": "Cash-on-Cash",
+};
+
+const isCalculatorRoute = computed(() => route.path.startsWith("/calculator")); // Check if current route for icon
+const pageTitle = computed(() => routeTitles[route.path] || ""); // Compute page title based on current route
 </script>
 
 <template>
   <v-app>
     <!-- Top App Bar -->
     <v-app-bar app color="primary" style="position: relative" dark>
+      <!-- Desktop View -->
       <template v-if="isDesktop">
         <v-btn icon @click="toggleDrawer" class="me-2">
           <v-icon color="white">mdi-menu</v-icon>
         </v-btn>
-        <v-app-bar-title>REI Tools by Cyberdime</v-app-bar-title>
+        <v-app-bar-title>
+          REI Tools
+          <span v-if="isCalculatorRoute">
+            |
+            <v-icon class="mx-1" size="20">mdi-calculator</v-icon>
+          </span>
+          <span v-if="pageTitle">{{ pageTitle }}</span>
+        </v-app-bar-title>
       </template>
+      <!-- Mobile View -->
       <template v-else>
-        <v-app-bar-title>REI Tools</v-app-bar-title>
+        <v-app-bar-title>
+          <span v-if="isCalculatorRoute">
+            <v-icon class="mx-1" size="20">mdi-calculator</v-icon>
+          </span>
+          <span v-if="pageTitle">{{ pageTitle }}</span>
+        </v-app-bar-title>
         <v-spacer />
         <v-btn icon @click="toggleDrawer">
           <v-icon color="white">mdi-menu</v-icon>
@@ -60,10 +90,13 @@ onUnmounted(() => {
         <!-- Calculator Group -->
         <v-list-group value="calculator" prepend-icon="mdi-calculator">
           <template #activator>
-            <v-list-item-title>Calculators</v-list-item-title>
+            <v-list-item-title
+              ><v-icon class="mx-1" size="20">mdi-calculator</v-icon
+              >Calculators</v-list-item-title
+            >
           </template>
           <v-list-item to="/calculator/standard" link @click="closeDrawer">
-            <v-list-item-title>Standard Calculator</v-list-item-title>
+            <v-list-item-title>Calculator</v-list-item-title>
           </v-list-item>
           <v-list-item to="/calculator/fix-flip" link @click="closeDrawer">
             <v-list-item-title>Fix & Flip</v-list-item-title>
